610909e36e0584d5235c95b9d93b278c671ca6c7
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-08-08 10:15:01 +0530

    [Riya] Add : Garbage Collection

--- GarbageCollection/Program.cs
+++ GarbageCollection/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+
+namespace GarbageCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long mem1 = GC.GetTotalMemory(false);
+            int[] values = new int[5000];
+            values = null;
+
+            long mem2 = GC.GetTotalMemory(false);
+            GC.Collect();
+
+            long mem3 = GC.GetTotalMemory(false);
+            Console.WriteLine("Mem1" + mem1);
+            Console.WriteLine("Mem2" + mem2);
+            Console.WriteLine("Mem3" + mem3);
+        }
+    }
+}
